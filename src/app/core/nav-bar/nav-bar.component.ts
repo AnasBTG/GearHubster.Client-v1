8b06ad72a58e0ea61eb2561df8bc50e14d393a6f
Anass Boutaghrassa <anass.boutaghrassa@gmail.com>
2023-12-10 11:23:28 +0100

    feat(products) : Add product items + update navbar

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  isMainNavbarFixed: boolean = false;
 
+  isSearchActive = false;
+
+  @HostListener('window:scroll', ['$event'])
+
+  onScroll() {
+    const mainNavbar = document.getElementById('mainNavBar');
+
+    if (mainNavbar)
+    {
+      this.isMainNavbarFixed = window.scrollY > mainNavbar.offsetHeight;
+    }
+  }
+
+  openSearch() {
+    this.isSearchActive = true;
+    console.log("Search opened");
+  }
+
+  closeSearch() {
+    this.isSearchActive = false;
+    console.log("Search opened");
+  }
+
+  pauseTicker(pause: boolean) {
+    const tickers = document.querySelectorAll('.infos-ticker') as NodeListOf<HTMLElement>;
+    tickers.forEach((ticker) => {
+      ticker.style.animationPlayState = pause ? 'paused' : 'running';
+    });
+  }
 }
